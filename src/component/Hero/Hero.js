ef34cb8dc55bd8a61e0a2a25c17c5c62e37c7230
nishadahmed5 <nishatahmed650@gmail.com>
2022-10-11 13:03:22 +0600

    home redesign

--- src/component/Hero/Hero.js
+++ src/component/Hero/Hero.js
@@ -1,42 +1,70 @@
 import React from 'react';
 import Lottie from 'lottie-react';
 import quiz from '../../assets/quiz.json'
-import bg from '../../assets/background.png'
+
 
 
 const Hero = () => {
     return (
-        <div className="relative flex flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0 bg-gradient-to-r from-purple-500 to-pink-500">
-        <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-12 lg:max-w-screen-xl">
-          <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-            <div className="max-w-xl mb-6">
-              <div>
-                <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-                  New Colaboration
-                </p>
+      <div className="bg-gradient-to-r from-purple-500 to-pink-500">
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+          <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
+            <a href="/" className="mb-6 sm:mx-auto">
+              <div className="flex items-center justify-center w-12 h-12 rounded-full bg-teal-accent-400">
+                <svg
+                  className="w-10 h-10 text-white"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
               </div>
-              <h2 className="max-w-lg mb-6 font-sans text-6xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                The quick, brown fox
-                <br className="hidden md:block" />
-                jumps over{' '}
-                <span className="inline-block text-deep-purple-accent-400">
-                  a lazy dog
-                </span>
+            </a>
+            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto">
+                <span className="relative inline-block">
+                  <svg
+                    viewBox="0 0 52 24"
+                    fill="currentColor"
+                    className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-deep-purple-accent-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                  >
+                    <defs>
+                      <pattern
+                        id="700c93bf-0068-4e32-aafe-ef5b6a647708"
+                        x="0"
+                        y="0"
+                        width=".135"
+                        height=".30"
+                      >
+                        <circle cx="1" cy="1" r=".7" />
+                      </pattern>
+                    </defs>
+                    <rect
+                      fill="url(#700c93bf-0068-4e32-aafe-ef5b6a647708)"
+                      width="52"
+                      height="24"
+                    />
+                  </svg>
+                  <span className="relative">The</span>
+                </span>{' '}
+                quick, brown fox jumps over a lazy dog
               </h2>
               <p className="text-base text-white md:text-lg">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                quae. explicabo.
+                accusantium doloremque rem aperiam, eaque ipsa quae.
               </p>
             </div>
           </div>
         </div>
-        <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
-        <Lottie 
-         className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full" animationData={quiz} loop={true} ></Lottie>
-          
-        </div>
       </div>
+    </div>
     );
 };
 
