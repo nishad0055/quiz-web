28d2473ebf6cf84bbc6380ceb11cda0e73c79001
nishadahmed5 <nishatahmed650@gmail.com>
2022-10-12 14:26:41 +0600

    taost add

--- src/component/QuizDetails/Quiz/Quiz.js
+++ src/component/QuizDetails/Quiz/Quiz.js
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
 import './Quiz.css'
 import {AiFillEye} from 'react-icons/ai'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = ({qu}) => {
     const {question, options, correctAnswer,} = qu;
     
     const [eye , setEye] = useState(false);
+
+    function handleClick (correctAnswer) {
+        setEye(correctAnswer);
+    }
     
 
   
         const selectOption = (option) =>{
             if(correctAnswer ===option){
-                alert('correct')
+                toast("Wow Your Answer is Correct!");
             }
             else{
-                alert(false)
+                toast("Ops Your Answer is inCorrect!");
             }
            
         }
@@ -26,12 +32,12 @@ const Quiz = ({qu}) => {
         <div className ='all-qu'>
             
             <h5 className='w-[60%] mx-auto text-center pt-5 text-2xl' >Question: {question.replace(/<[^>]+>/g, '')} </h5>
-               
-            <div  className ='flex items-center justify-center cursor-pointer' >
-            <AiFillEye ></AiFillEye>
-        
-
+            
+            <div className='flex justify-center items-center'>
+            <button> <AiFillEye  onClick={handleClick} ></AiFillEye></button>
             </div>
+          
+          
 
             <div className="qu-container">
                 <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-[60%] mx-auto gap-5 my-10">
@@ -45,6 +51,17 @@ const Quiz = ({qu}) => {
                  
                </ul>
             </div>
+            <ToastContainer
+               position="top-center"
+               autoClose={1000}
+               hideProgressBar={false}
+               newestOnTop={false}
+               closeOnClick
+               rtl={false}
+               pauseOnFocusLoss
+               draggable
+               pauseOnHover
+               />
         </div>
     );
 };
--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+ import  {ResponsiveContainer, BarChart, Bar , XAxis, YAxis ,Tooltip} from 'recharts'
 
 const Statistics = () => {
     const data = useLoaderData();
     console.log(data.data);
     return (
-        <div>
-           <h1>Data </h1>
-          {
-             
-                  <LineChart width={500} height={300} data ={data}>
-                      <XAxis dataKey= {data.length} />
-                   <YAxis  dataKey= 'name' />
-                      
-                  </LineChart>
-                
-          }
+        <div className='w-[90%] mx-auto justify-center items-center my-10' >
+          <ResponsiveContainer width= '100%'  aspect={3}>
+             <BarChart data ={data.data} width={600} height ={400} >
+                <XAxis dataKey= 'name' ></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+                <Bar dataKey="total" fill='blue' ></Bar>
+             </BarChart>
+          </ResponsiveContainer>
 
+          <h3 className='text-center text-2xl'>Quizes Barchart</h3>
            
         </div>
     );
